77dc8e558e0c487bb3190cfa4a67d9921572fa53
Leon Weber <leon@leonweber.de>
2013-05-29 16:12:13 +0200

    Assmann.py feeds words into the chain

--- Aßmann.py
+++ Aßmann.py
@@ -1,11 +1,35 @@
 #!/usr/bin/python3
 
+import re
+import sys
+import pickle
+
 import MarkovChain
 
-chain = MarkovChain.MarkovChain(8)
-chain.learn('choochoooshoe')
-print("Done with learning.")
-for i in range(20):
+pattern = """(\w+|\s+|,|\.|\?|!|"|'|\[|\]|\(|\)|\n)"""
+
+infile = sys.argv[1]
+order = sys.argv[2]
+chars = sys.argv[3]
+dumpname = '{}-{}-assmann.chain'.format(infile,order)
+
+try:
+    with open(dumpname, 'rb') as f:
+        chain = pickle.load(f)
+        print("Reading from {}".format(dumpname))
+except OSError:
+    def source():
+        with open(infile) as f:
+            yield from (m.group(0) for m in re.finditer(pattern, f.read()))
+
+    print("Learning.")
+    chain = MarkovChain.MarkovChain(order)
+    chain.learn(source())
+    print("Done learning!")
+    with open(dumpname, 'wb') as f:
+        pickle.dump(chain, f)
+
+for i in range(int(chars)):
     try:
         print(next(chain.emit()), end='')
     except StopIteration:
