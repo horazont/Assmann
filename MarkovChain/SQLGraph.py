75418109a908603bc932bd5b5e88f5a035859dda
Jonas Wielicki <j.wielicki@sotecware.net>
2013-06-04 18:34:30 +0200

    Optimize SQLGraph for a much better performance

--- MarkovChain/SQLGraph.py
+++ MarkovChain/SQLGraph.py
@@ -12,25 +12,69 @@ from sqlalchemy.ext.compiler import compiles
 import MarkovChain.Graph
 
 class InsertOrUpdateEdge(Insert):
-    def __init__(self, edge_table, node_id_a, node_id_b):
-        super().__init__(edge_table, values={
-            "node_a": node_id_a,
-            "node_b": node_id_b,
-            "weight": 1
-        })
+    def __init__(self, edge_table):
+        dct = {}
+        for col in edge_table.columns:
+            dct[col.name] = ''
+        dct["weight"] = 1
+
+        super().__init__(edge_table, values=dct)
+
+    def set_nodes(self, node_a, node_b):
+        dct = {}
+        for i, va, vb in zip(itertools.count(), node_a, node_b):
+            if va is None:
+                va = ''
+            if vb is None:
+                vb = ''
+            dct["v"+str(i)+"a"] = va
+            dct["v"+str(i)+"b"] = vb
+        return self.values(**dct)
 
 @compiles(InsertOrUpdateEdge)
 def compile_insert_or_update(insert, compiler, **kwargs):
     s = compiler.visit_insert(insert, **kwargs)
     return s + " ON DUPLICATE KEY UPDATE weight = weight + 1"
 
+
+class CollectEdges:
+    def __init__(self, conn, edge_table, count=1024):
+        self._max_count = count
+        self._edges = []
+        self._conn = conn
+        self._insert = InsertOrUpdateEdge(edge_table)
+
+    def add(self, node_a, node_b):
+        dct = {}
+        for i, va, vb in zip(itertools.count(), node_a, node_b):
+            if va is None:
+                va = ''
+            if vb is None:
+                vb = ''
+            dct["v"+str(i)+"a"] = va
+            dct["v"+str(i)+"b"] = vb
+        dct["weight"] = 1
+        self._edges.append(dct)
+
+        if len(self._edges) >= self._max_count:
+            self.flush()
+
+    def flush(self):
+        if len(self._edges) == 0:
+            return
+        self._conn.execute(
+            self._insert,
+            self._edges)
+        self._edges = []
+
 class SQLMarkovGraph(MarkovChain.Graph.AbstractMarkovGraph):
     def __init__(self, order, url,
             engine_and_conn=(None, None),
-            max_word_length=127,
+            max_word_length=63,
             debug=False):
         super().__init__()
-        assert order > 0
+        if order is not None and order <= 0:
+            raise ValueError("order must be a positive integer.")
         self._order = order
         if engine_and_conn != (None, None):
             if url is not None:
@@ -45,7 +89,6 @@ class SQLMarkovGraph(MarkovChain.Graph.AbstractMarkovGraph):
                 echo=debug
                 )
         self._metadata = MetaData()
-        self._node_column_names = ['v'+str(i) for i in range(order)]
 
         order_from_database = self._order_from_db()
         if (    order_from_database is not None and
@@ -58,51 +101,37 @@ class SQLMarkovGraph(MarkovChain.Graph.AbstractMarkovGraph):
                              ))
 
         if order is None:
+            if order_from_database is None:
+                raise ValueError("Could not get order from database and"
+                                 " no order was given.")
             self._order = order_from_database
+            order = self._order
+
+        self._src_column_names = ['v'+str(i)+"a" for i in range(order)]
+        self._dst_column_names = ['v'+str(i)+"b" for i in range(order)]
 
         args = [Column(
                     col,
-                    String(max_word_length),
-                    nullable=True)
-                for col in self._node_column_names]
-        args.append(
-            UniqueConstraint(*self._node_column_names)
-        )
-
-        self._nodes = Table(
-            'nodes', self._metadata,
-            Column('node_id', Integer, primary_key=True),
-            *args
-        )
+                    Unicode(max_word_length),
+                    nullable=True,
+                    primary_key=True)
+                for col in self._src_column_names + self._dst_column_names]
 
         self._edges = Table(
             'edges', self._metadata,
-            Column('node_a',
-                   None,
-                   ForeignKey('nodes.node_id'),
-                   nullable=False,
-                   primary_key=True),
-            Column('node_b',
-                   None,
-                   ForeignKey('nodes.node_id'),
-                   nullable=False,
-                   primary_key=True),
             Column('weight',
                    Integer,
-                   nullable=False)
+                   nullable=False),
+            *args
         )
 
+        self._src_columns = [getattr(self._edges.c, col)
+                             for col in self._src_column_names]
+        self._dst_columns = [getattr(self._edges.c, col)
+                             for col in self._dst_column_names]
+
         self._metadata.create_all(self._engine)
-
-        self._node_columns = [
-            getattr(self._nodes.c, col)
-            for col in self._node_column_names
-        ]
-
-        self._fast_get_node_id = \
-            functools.lru_cache()(lambda x: self._get_node_id(x))
-
-        self._buffer = []
+        self._collect = CollectEdges(self._conn, self._edges)
 
     @staticmethod
     def _engine_and_conn_for_url(url, echo=False):
@@ -112,92 +141,44 @@ class SQLMarkovGraph(MarkovChain.Graph.AbstractMarkovGraph):
 
     def _order_from_db(self):
         try:
-            tbl = Table("nodes",
+            tbl = Table("edges",
                         MetaData(),
                         autoload=True,
                         autoload_with=self._engine)
         except sqlalchemy.exc.NoSuchTableError:
             return None
         else:
-            return len(list(filter(lambda x: x.name.startswith("v"),
+            return len(list(filter(lambda x: x.name.startswith("v") and x.name.endswith("a"),
                                    tbl.columns)))
 
-    def _node_where(self, node):
-        iterable = iter(zip(self._node_columns, node))
-        col, val = next(iterable)
-        clause = col == val
-        for col, val in iterable:
-            clause &= col == val
-        return clause
-
-    def _get_node_id(self, node):
-        result = self._conn.execute(sql.select(
-            [self._nodes.c.node_id],
-            whereclause=self._node_where(node))).fetchone()
-        if result is not None:
-            return result[0]
-        else:
-            return None
-
-    def _has_link(self, node_id_a, node_id_b):
-        return self._conn.execute(sql.select(
-            [self._edges.c.weight],
-            whereclause=\
-                (self._edges.c.node_a == node_id_a) &
-                (self._edges.c.node_b == node_id_b))).fetchone() is not None
-
-    def _add_node(self, node):
-        self._fast_get_node_id.cache_clear()
-        statement = self._nodes.insert().values(
-            **dict(zip(self._node_column_names, node))
-        ) #.returning(self._nodes.c.node_id)
-
-        return self._conn.execute(statement).lastrowid
-
-    def _increase_weight(self, node_id_a, node_id_b):
-        self._conn.execute(self._edges.update().
-            where((self._edges.c.node_a == node_id_a) &
-                  (self._edges.c.node_b == node_id_b)).
-            values(self._edges.c.weight + 1))
-
-    def _add_link(self, node_id_a, node_id_b):
-        self._conn.execute(self._edges.insert().
-            values(node_a=node_id_a,
-                   node_b=node_id_b,
-                   weight=1))
-
     def add_transition(self, src, dst):
-        node_a = self._fast_get_node_id(src)
-        if node_a is None:
-            node_a = self._add_node(src)
-
-        node_b = self._fast_get_node_id(dst)
-        if node_b is None:
-            node_b = self._add_node(dst)
-
-        statement = InsertOrUpdateEdge(self._edges, node_a, node_b)
-
-        self._conn.execute(statement)
+        self._collect.add(src, dst)
 
     def _recombine_node(self, items):
-        node = tuple(itertools.islice(items, 0, self._order))
+        node = self._recreate_node(itertools.islice(items, 0, self._order))
         weight = items[self._order]
         return node, weight
 
+    @staticmethod
+    def _where_node(columns, node):
+        iterator = iter(zip(columns, node))
+        col, val = next(iterator)
+        clause = col == val
+        for col, val in iterator:
+            clause &= col == val
+        return clause
+
+    @staticmethod
+    def _recreate_node(row):
+        return tuple(x.decode("utf-8") for x in row)
+
     def get_weighted_transitions(self, src):
+        self._collect.flush()
         return map(
             self._recombine_node,
             self._conn.execute(
-                select(
-                    self._node_columns + [self._edges.c.weight],
-                    from_obj=[
-                        self._edges.join(
-                            self._nodes,
-                            self._nodes.c.node_id == self._edges.c.node_b
-                        )
-                    ],
-                    whereclause=self._edges.c.node_a == self._fast_get_node_id(src)
-                )
+                select(self._dst_columns + [self._edges.c.weight])
+                .where(self._where_node(self._src_columns, src))
             )
             )
 
@@ -208,20 +189,24 @@ class SQLMarkovGraph(MarkovChain.Graph.AbstractMarkovGraph):
         if random_choice is None:
             # we'll try to offload the random choice to the DB server
             count = self._conn.execute(
-                        select([func.count()]).
-                            from_table(self._nodes)
-                        ).fetchone()[0]
+                        select([func.count()])
+                            .select_from(self._edges)
+                            .group_by(*self._src_columns)
+                        )
+            count = len(list(count))
             node = tuple(self._conn.execute(
-                             self._nodes.select(self._node_columns).
-                                         offset(random.randint(count))
-                             ).fetchone())
+                select(self._src_columns)
+                    .group_by(*self._src_columns)
+                    .offset(random.randint(0, count))
+                ).fetchone())
             return node
         else:
             # okay, *this* will be horribly slow
-            nodes = list(map(tuple, self._conn.execute(
-                self._nodes.select()
-                )))
-            return random_choice(nodes)
+            nodes = list(self._conn.execute(
+                select(self._src_columns).
+                group_by(*self._src_columns)
+                ))
+            return self._recreate_node(random_choice(nodes))
 
     @property
     def order(self):
@@ -233,6 +218,6 @@ class SQLMarkovGraph(MarkovChain.Graph.AbstractMarkovGraph):
             url,
             debug=debug)
 
-    def flush(self):
-        pass
+    def flush(self, url=None):
+        self._collect.flush()
 
